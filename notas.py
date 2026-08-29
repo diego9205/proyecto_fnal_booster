5bf2a9aab18617ab63cc8fa8bbd65d3e30c8b638
Diegoortiz <diego.ortiz.hdez.07@gmail.com>
2026-08-28 20:39:25 -0600

    se agregaron funciones de ver y agregar

--- notas.py
+++ notas.py
@@ -1,6 +1,34 @@
 from pathlib import Path
 from datetime import datetime
+import json
 print("------Bienvenido al sistema de notas------  \n")
+
+notas = []
+def agregar_nota():
+    tiempo = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    titulo = input("Ingrese el título de la nota: ")
+    contenido = input("Ingrese el contenido de la nota: ")
+    categoria = input("Ingrese la categoría de la nota: ")
+
+    nota = {
+        "titulo": titulo,
+        "contenido": contenido,
+        "categoria": categoria,
+        "fecha": tiempo
+    }
+    notas.append(nota)
+
+def ver_notas():
+    if not notas:
+        print("No hay notas ")
+
+    else:
+        for note in notas:
+            print(f"Título: {note['titulo']}")
+            print(f"Contenido: {note['contenido']}")
+            print(f"Categoría: {note['categoria']}")
+            print(f"Fecha: {note['fecha']}")
+            print("------------------------")
 
 
 menu = """
@@ -23,8 +51,12 @@ while opc != 7:
     opc = int(input(menu)) 
     if opc == 1:
         print("Agregar nota")
+        agregar_nota()
+
     elif opc == 2:
         print("Ver notas")
+        ver_notas()
+
     elif opc == 3:
         print("Ver nota")
 
@@ -42,4 +74,5 @@ while opc != 7:
 
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
+
 
